42aeb39c5907d8914a111d47d8dd9838f3a86fb8
Pravin Kumar Lakshmanaperumal <lakpravi@amazon.com>
2020-12-13 21:00:05 +0000

    [Add] Go lang template used for LeetCode questions

---
+++ template.go
@@ -0,0 +1,17 @@
+// This code is the solution to the LeetCode problem - [Paste LeetCode Link] 
+//
+// Problem Statement:
+// Mention Problem Statement Here.
+//
+// Input/Output:
+//
+// Mention Input/Output Here.
+
+package main  
+ 
+import "fmt"
+
+func main() {
+	fmt.Println("This is a sample template used for LeetCode Questions")
+}
+
